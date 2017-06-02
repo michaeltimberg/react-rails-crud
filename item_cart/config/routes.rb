7b0ad204c7ce4bd895135d0cf9d9c189c46e22e5
Michael Timberg <timberg.1@osu.edu>
2017-06-02 12:09:04 -0400

    Routing the controllers

--- item_cart/config/routes.rb
+++ item_cart/config/routes.rb
@@ -1,3 +1,18 @@
+#The routing for the controller has to consider the fact that it’s within two namespaces; API and V1. We’ll do this using the namespace method.
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'site#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [
+          :index,
+          :create,
+          :destroy,
+          :update
+      ]
+    end
+  end
 end
