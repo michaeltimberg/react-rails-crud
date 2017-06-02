88ca80a6f10a10b98a72c3c27cb51e1230820f9e
Michael Timberg <timberg.1@osu.edu>
2017-06-02 15:33:37 -0400

    The first component

---
+++ item_cart/app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,11 @@
+// The first thing we need to do is to set-up a jsx file in our components folder.
+
+var Main = React.createClass({
+    render () {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+});
